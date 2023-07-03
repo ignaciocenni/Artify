fdbf26ba7f851dd2cf7a9b84655b931254c2845f
EnzoGabrielSb <enzogabrielvera07@gmail.com>
2023-07-03 20:30:49 -0300

    feat(app/api/product): create product POST

--- app/api/product/route.js
+++ app/api/product/route.js
@@ -1,5 +1,31 @@
 import { NextResponse } from "next/server";
 import { data } from "../response";
-export async   function GET(){
-    return NextResponse.json(data)
+
+export async function GET() {
+  return NextResponse.json(data);
+}
+
+export async function POST(req) {
+  const { name, description, price, stock, image, userId, categoryId } =
+    req.json();
+  const newProduct = await prisma.User.create({
+    data: {
+      name,
+      description,
+      price,
+      stock,
+      image,
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+      category: {
+        connect: {
+          id: categoryId,
+        },
+      },
+    },
+  });
+  return NextResponse.json(newProduct);
 }
