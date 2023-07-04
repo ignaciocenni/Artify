6083e46cd856bc7f9ad1c3edd9f4a053eed2252f
EnzoGabrielSb <enzogabrielvera07@gmail.com>
2023-07-04 19:58:30 -0300

    feat(app/api/product): handlers routes completos con validaciones y controladores completos (revisar antes de aceptar)

---
+++ app/api/product/controllers.js
@@ -0,0 +1,54 @@
+import prisma from "../db/client";
+
+// Seria necesario incluir los usuarios?
+const allProducts = async () => {
+  const response = await prisma.Product.findMany();
+  return response;
+};
+
+const addProduct = async (
+  name,
+  description,
+  price,
+  stock,
+  image,
+  userId,
+  categoryId
+) => {
+  if (
+    !name ||
+    !description ||
+    !price ||
+    !stock ||
+    !image ||
+    !userId ||
+    !categoryId
+  )
+    throw new Error("Missing arguments");
+
+  const newProduct = await prisma.Product.create({
+    data: {
+      name,
+      description,
+      price,
+      stock,
+      image,
+      userId,
+      categoryId,
+    },
+  });
+
+  return newProduct;
+};
+
+const productDelete = async (id) => {
+  if (isNaN(id)) throw new Error("Id must be a number");
+
+  const searchIdProd = await prisma.Product.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+};
+
+export { allProducts, addProduct, productDelete };
