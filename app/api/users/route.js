b9c9933ece7fa08a64bab0e7c70242d82582d9f2
ignaciocenni <ignaziocenni@gmail.com>
2023-07-04 16:53:13 -0300

    feat(api/users route.js): add handlers PUT and DELETE

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
-import { allUsers, addUser } from "./controllers";
+import { allUsers, addUser, updateUser, deleteUser } from "./controllers";
 
 export async function GET() {
   try {
-    const res = await allUsers();
-    return NextResponse.json(res, { status: 200 });
+    const response = await allUsers();
+    return NextResponse.json(response, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 404 });
   }
@@ -16,6 +16,28 @@ export async function POST(request) {
   try {
     const newUser = await addUser(name, email, password, rol);
     return NextResponse.json(newUser, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 404 });
+  }
+}
+
+export async function PUT(request) {
+  const { id, name, email, password } = await request.json();
+
+  try {
+    const user = await updateUser(id, name, email, password);
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 404 });
+  }
+}
+
+export async function DELETE(request) {
+  const { id } = await request.json();
+
+  try {
+    await deleteUser(id);
+    return NextResponse.json({ message: "User deleted" }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 404 });
   }
