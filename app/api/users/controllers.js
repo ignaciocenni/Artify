b875ae8cfeac0f08a21909ef0a92e7f4bb25aaf8
ignaciocenni <ignaziocenni@gmail.com>
2023-07-04 16:54:29 -0300

    feat(api/users controllers.js): add  functions updateUser and deleteUser

--- app/api/users/controllers.js
+++ app/api/users/controllers.js
@@ -1,19 +1,18 @@
 import prisma from "../db/client";
 
 const allUsers = async () => {
-  const res = await prisma.User.findMany({
+  const response = await prisma.User.findMany({
     include: {
       products: true,
     },
   });
-  if (!res.length) {
-    throw new Error("Users not found");
-  } else return res;
+
+  if (!response.length) throw new Error("Users not found");
+  return response;
 };
 
 const addUser = async (name, email, password, rol) => {
-  if (!name || !email || !password || !rol)
-    throw new Error("Arguments missing");
+  if (!name || !email || !password || !rol) throw new Error("Missing arguments");
 
   const searchedUser = await prisma.User.findFirst({ where: { email } });
   if (searchedUser === null) {
@@ -30,4 +29,38 @@ const addUser = async (name, email, password, rol) => {
   } else throw new Error("User already exists");
 };
 
-export { allUsers, addUser };
+const updateUser = async (id, name, email, password) => {
+  if (!id && !name && !email && !password) throw new Error("Missing arguments");
+
+  const user = await prisma.User.update({
+    where: {
+      id: id,
+    },
+    data: {
+      name: name,
+      email: email,
+      password: password,
+    },
+  });
+
+  return user;
+};
+
+const deleteUser = async (id) => {
+  if (!id) throw new Error("Missing id");
+  if (typeof id !== "number") throw new Error("Id must be a number");
+
+  const searchedUser = await prisma.User.findFirst({ where: { id } });
+
+  if (searchedUser === null) throw new Error("User not found");
+
+  const user = await prisma.User.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  return user;
+};
+
+export { allUsers, addUser, updateUser, deleteUser };
