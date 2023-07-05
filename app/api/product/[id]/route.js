4ea9f352746f5334324074a3661c052b2836933f
ignaciocenni <ignaziocenni@gmail.com>
2023-07-05 18:27:02 -0300

    feat(api/product/[id] route): add GET for ID and DELETE for ID

--- app/api/product/[id]/route.js
+++ app/api/product/[id]/route.js
@@ -1,10 +1,24 @@
-// ruta dinamica de productos:
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import { getProduct, deleteProduct } from "./controllers";
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const { id } = req.params;
+export async function GET(request, { params }) {
+  const id = +params.id;
 
-  return NextResponse.json({
-    id: id,
-  });
+  try {
+    const response = await getProduct(id);
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 404 });
+  }
+}
+
+export async function DELETE(request, { params }) {
+  const id = +params.id;
+
+  try {
+    const response = await deleteProduct(id);
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 404 });
+  }
 }
