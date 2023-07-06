43355206769ed6272f5d5bb00c4bf9cf5ada4420
Tomás <t.diaz.soto@gmail.com>
2023-07-05 21:11:58 -0400

    fix(api/product route.js): fix validations

--- app/api/product/route.js
+++ app/api/product/route.js
@@ -1,12 +1,24 @@
 import { NextResponse } from "next/server";
-import { addProduct, allProducts, getAllProducts, updateProduct } from "./controllers";
+import {
+  addProduct,
+  allProducts,
+  getAllProducts,
+  updateProduct,
+} from "./controllers";
 import { deleteProduct, productDelete } from "./[id]/controllers";
 
 export async function PUT(request) {
   const { id, name, description, price, stock, image } = await request.json();
 
   try {
-    const response = await updateProduct(id, name, description, price, stock, image);
+    const response = await updateProduct(
+      id,
+      name,
+      description,
+      price,
+      stock,
+      image
+    );
     return NextResponse.json(response, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 404 });
@@ -23,11 +35,15 @@ export async function GET() {
 }
 
 export async function POST(req) {
-  const { name, description, price, stock, image, userId, categoryId } = await req.json();
+  const { name, description, price, stock, image, userId, categoryId } =
+    await req.json();
 
   const nameRegex = /^[A-Za-z\s]+$/;
-  if (nameRegex.test(name)) {
-    return NextResponse.json({ error: "The must be a normal name..." }, { status: 400 });
+  if (!nameRegex.test(name)) {
+    return NextResponse.json(
+      { error: "The must be a normal name..." },
+      { status: 400 }
+    );
   }
   if (description.length <= 12) {
     return NextResponse.json(
@@ -35,22 +51,36 @@ export async function POST(req) {
       { status: 400 }
     );
   }
-  if (price >= 0) {
+  if (price <= 0) {
     return NextResponse.json(
       { error: "Price cannot be less than or equal to $0" },
       { status: 400 }
     );
   }
-  if (stock > 0) {
-    return NextResponse.json({ error: "Price cannot be less than 0" }, { status: 400 });
+  if (stock < 0) {
+    return NextResponse.json(
+      { error: "Price cannot be less than 0" },
+      { status: 400 }
+    );
   }
-  const imageRegex = /\.(jpg|jpeg|png)$/i;
-  if (imageRegex.test(image)) {
-    return NextResponse.json({ error: "The image format must be valid." }, { status: 400 });
-  }
+  // const imageRegex = /\.(jpg|jpeg|png)$/i;
+  // if (!imageRegex.test(image)) {                              Cambiar por un regex de url
+  //   return NextResponse.json(
+  //     { error: "The image format must be valid." },
+  //     { status: 400 }
+  //   );
+  // }
 
   try {
-    const newProduct = await addProduct(name, description, price, stock, image, userId, categoryId);
+    const newProduct = await addProduct(
+      name,
+      description,
+      price,
+      stock,
+      image,
+      userId,
+      categoryId
+    );
     return NextResponse.json(newProduct, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 404 });
@@ -62,7 +92,10 @@ export async function DELETE(request) {
 
   try {
     await deleteProduct(id);
-    return NextResponse.json({ message: `Product with id: ${id} deleted` }, { status: 200 });
+    return NextResponse.json(
+      { message: `Product with id: ${id} deleted` },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 404 });
   }
