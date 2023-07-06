748c014254ba2a0928940c3db198a9295e00dd29
EMelendezCamacho <eduardomelendezc5@gmail.com>
2023-07-05 22:26:29 -0600

    fix(components/Card&Cards.jsx):searreglo el codigo para que recibiera la data del back

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Card from "./Card";
 import { publicacionesArtesania } from "./databs";
-export default function Cards() {
+export default function Cards({data}) {
+  const info = data[0]
+  console.log(info);
   return (
     <div className="grid grid-cols-4 w-1/2 px-2">
-      {publicacionesArtesania.map((data) => (
+      {info.map((resp) => (
         <Card
-          key={data.id}
-          id={data.id}
-          image={data.imagenProducto}
-          user={data.username}
-          price={data.precio}
-          title={data.titulo}
-          userImage={data.imagenUsuario}
+        key={resp.id}
+        id={resp.id}
+        image={resp.image}
+        user={resp.name}
+        price={resp.price}
+        title={resp.name}
+        userImage={resp.image}
         />
       ))}
     </div>
