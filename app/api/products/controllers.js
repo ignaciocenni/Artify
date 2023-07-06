5e19ef01268dea55ff5286a52464a2152ba2b9e0
Tomás <t.diaz.soto@gmail.com>
2023-07-06 17:44:20 -0400

    fix(api/products controllers.js): fix validations

--- app/api/products/controllers.js
+++ app/api/products/controllers.js
@@ -25,26 +25,21 @@ const getAllProducts = async (name) => {
   return response;
 };
 
-const addProduct = async (
-  name,
-  description,
-  price,
-  stock,
-  image,
-  userId,
-  categoryId
-) => {
-  if (
-    !name ||
-    !description ||
-    !price ||
-    !stock ||
-    !image ||
-    !userId ||
-    !categoryId
-  )
+const addProduct = async (name, description, price, stock, image, userId, categoryId) => {
+  if (!name || !description || !price || !stock || !image || !userId || !categoryId)
     throw new Error("Missing arguments");
+  const nameRegex = /^[a-zA-Z0-9\s.,]*$/;
+  if (!nameRegex.test(name)) throw new Error("The must be a normal name...");
+  if (description.length <= 12) throw new Error("The description must be more of 12 letters.");
+  if (price <= 0) throw new Error("Price cannot be less than or equal to $0");
 
+  // const imageRegex = /\.(jpg|jpeg|png)$/i;
+  // if (!imageRegex.test(image)) {
+  //   return NextResponse.json(
+  //     { error: "The image format must be valid." },
+  //     { status: 400 }
+  //   );
+  // }
   const newProduct = await prisma.Product.create({
     data: {
       name,
