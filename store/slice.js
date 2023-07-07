7d1ef63e13799b1e3f6a3b86796d7c866eeeef9e
EMelendezCamacho <eduardomelendezc5@gmail.com>
2023-07-07 00:54:04 -0600

    fix(store/slice.js):arreglar estados y nuevo reducer

--- store/slice.js
+++ store/slice.js
@@ -3,14 +3,29 @@ export const Slice = createSlice({
     name: 'valores',
     initialState: {
         nombre: [],
-        
+        copyProducts: []
+
     },
 
     reducers: {
         GET_INFO: (state, action) => {
             state.nombre = [action.payload];
+            state.copyProducts = [action.payload];
         },
-        
-}})
+        countrie: (state, action) => {
+            const countrie = [...state.nombre[0]]
+            const find = countrie.filter(element => element.countrie === action.payload)
+            state.products = [find]
+            console.log(countrie, "reducer");
+        },
+        price: (state, { type, payload }) => {
+            const price = [...state.nombre[0]]
+            const find = price.filter(function (num) {
+                return num.price >= payload[0] && num.price <= payload[1];
+            });
+            state.nombre=[find]
+        }
+    }
+})
 
-export const { GET_INFO} = Slice.actions
+export const { GET_INFO, countrie, price } = Slice.actions
