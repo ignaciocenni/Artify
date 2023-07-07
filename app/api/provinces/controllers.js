cb8dab5dd4269a1d067747319a7dec9afdb457fa
EnzoGabrielSb <enzogabrielvera07@gmail.com>
2023-07-07 18:36:17 -0300

    feat(api/loadData): creatingLoadData

--- app/api/provinces/controllers.js
+++ app/api/provinces/controllers.js
@@ -5,36 +5,4 @@ const allProvinces = async () => {
   return response;
 };
 
-const postAllProvinces = async () => {
-  const allProvinces = await prisma.Province.createMany({
-    data: [
-      { name: "Buenos Aires" },
-      { name: "Catamarca" },
-      { name: "Chaco" },
-      { name: "Chubut" },
-      { name: "Córdoba" },
-      { name: "Corrientes" },
-      { name: "Entre Ríos" },
-      { name: "Formosa" },
-      { name: "Jujuy" },
-      { name: "La Pampa" },
-      { name: "La Rioja" },
-      { name: "Mendoza" },
-      { name: "Misiones" },
-      { name: "Neuquén" },
-      { name: "Río Negro" },
-      { name: "Salta" },
-      { name: "San Juan" },
-      { name: "San Luis" },
-      { name: "Santa Cruz" },
-      { name: "Santa Fe" },
-      { name: "Santiago del Estero" },
-      { name: "Tierra del Fuego" },
-      { name: "Tucumán" },
-    ],
-    skipDuplicates: true,
-  });
-  return { message: "All provinces upload(?" };
-};
-
-export { allProvinces, postAllProvinces };
+export { allProvinces };
--- app/api/provinces/route.js
+++ app/api/provinces/route.js
@@ -9,12 +9,3 @@ export async function GET() {
     return NextResponse.json({ error: error.message }, { status: 404 });
   }
 }
-
-export async function POST() {
-  try {
-    const response = await postAllProvinces();
-    return NextResponse.json(response, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 404 });
-  }
-}
