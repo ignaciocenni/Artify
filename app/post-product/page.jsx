9589dcc7980b57b2266adb79117b9ca6582d09fb
ignaciocenni <ignaziocenni@google.com>
2023-07-06 23:00:42 -0300

    fix

--- app/post-product/page.jsx
+++ app/post-product/page.jsx
@@ -3,64 +3,58 @@
 import { useState } from "react";
 import axios from "axios";
 
-const postProduct = async (form) =>{
- try {
-   const response = await axios.post("http://localhost:3000/api/products", form)
-   console.log(response.data);
- } catch (error) {
+const postProduct = async (form) => {
+  try {
+    const response = await axios.post("http://localhost:3000/api/products", form);
+    console.log(response.data);
+  } catch (error) {
     console.log(error.message);
- }
-}
-export default function page() {
-    
-
-    const [form, setForm] = useState({
+  }
+};
+export default function Page() {
+  const [form, setForm] = useState({
     name: "",
     description: "",
     price: "",
     stock: "",
     image: "",
-    categoryId:"",
-    userId:""
-    });
+    categoryId: "",
+    userId: "",
+  });
   const handleClick = () => {
-    event.preventDefault()
-    postProduct(form)
+    event.preventDefault();
+    postProduct(form);
     console.log("Producto creado con exito");
   };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-  
+
     let parsedValue = value;
     if (name === "price") {
       parsedValue = parseFloat(value);
-    } else if (
-      name === "stock" ||
-      name === "categoryId" ||
-      name === "userId"
-    ) {
+    } else if (name === "stock" || name === "categoryId" || name === "userId") {
       parsedValue = parseInt(value, 10);
     }
-  
+
     setForm({ ...form, [name]: parsedValue });
   };
 
   return (
     <div className=" text-center h-[100vh] grid justify-center items-center">
       <form>
-        <h1 className="font-semibold text-3x1">
-          Porfavor ingrese los datos de su producto!
-        </h1>
+        <h1 className="font-semibold text-3x1">Porfavor ingrese los datos de su producto!</h1>
         <br />
         <div className="mb-4">
-
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username"
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
             type="text"
             placeholder="Nombre del producto"
             onChange={handleChange}
             name="name"
-            value={form.name} />
+            value={form.name}
+          />
         </div>
         <div className="mb-4">
           <input
@@ -130,17 +124,15 @@ export default function page() {
           />
         </div>
         <div className="flex items-center justify-center">
-            <button
-              className="bg-[var(--detail)] hover:bg-[var(--background-sec)] hover:text-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-[250px]"
-              onClick={handleClick}
-            >
-              Continuar
-            </button>
+          <button
+            className="bg-[var(--detail)] hover:bg-[var(--background-sec)] hover:text-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-[250px]"
+            onClick={handleClick}
+          >
+            Continuar
+          </button>
         </div>
       </form>
-      <p className="text-center text-gray-500 text-xs">
-        Apify. All rights reserved.
-      </p>
+      <p className="text-center text-gray-500 text-xs">Apify. All rights reserved.</p>
     </div>
   );
 }
