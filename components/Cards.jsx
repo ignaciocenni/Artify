8439540d502dcfc8a3c9e9026b283012975d25d0
EMelendezCamacho <eduardomelendezc5@gmail.com>
2023-07-08 09:59:54 -0600

    fic(components/cards&card)se agrega un loading

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,23 +1,33 @@
 "use client";
 import Card from "./Card";
+import Suspense from'react'
+import Loading from "@/app/loading";
 
 export default function Cards({ products }) {
   const response = products
   console.log(response);
   return (
     <div className="grid grid-cols-4 w-1/2 px-2">
-      {response &&
-        response.map((resp) => (
-          <Card
-            key={resp.id}
-            id={resp.id}
-            image={resp.image}
-            user={`${resp.user.name} ${resp.user.lastName}`}
-            price={resp.price}
-            title={resp.name}
-            userImage={resp.user.image}
-          />
-        ))}
+      {!response.length? (
+            <Loading/>// Renderiza el mensaje de error
+          ) : (
+            response.map((resp) => (
+          
+              <Card
+                key={resp.id}
+                id={resp.id}
+                image={resp.image}
+                user={`${resp.user.name} ${resp.user.lastName}`}
+                price={resp.price}
+                title={resp.name}
+                userImage={resp.user.image}
+              />
+            ))
+          )}
+   
+
+      
+
     </div>
   );
 }
