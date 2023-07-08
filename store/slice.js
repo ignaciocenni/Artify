a764b8303e93b91faead2e93b59d1e6b2cb7c7c9
EnzoGabrielSb <enzogabrielvera07@gmail.com>
2023-07-08 20:19:53 -0300

    feat(components/ filter.jsx&slice.js): add Filter by Provinces.

--- store/slice.js
+++ store/slice.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { revalidatePath } from "next/cache";
+
 export const Slice = createSlice({
   name: "valores",
   initialState: {
     nombre: [],
     copyProducts: [],
+    provinces: [],
   },
 
   reducers: {
@@ -12,18 +13,29 @@ export const Slice = createSlice({
       state.nombre = action.payload;
       state.copyProducts = action.payload;
     },
+    GET_PROVINCES: (state, action) => {
+      state.provinces = action.payload;
+    },
+
     price: (state, { type, payload }) => {
-      const price = [...state.nombre[0]];
+      const price = [...state.nombre];
       const find = price.filter(function (num) {
         return num.price >= payload[0] && num.price <= payload[1];
       });
-      state.nombre = [find];
+      state.nombre = find;
     },
+
     countrie: (state, action) => {
-      const countrie = [...state.nombre[0]];
-      const find = countrie.filter((element) => element.countrie === action.payload);
-      state.nombre = [find];
+      console.log(action.payload);
+      const countrie = [...state.nombre];
+      console.log("Provincias Redux:", countrie);
+      const find = countrie.filter(
+        (element) => element.user.province.name === action.payload
+      );
+      console.log(find);
+      state.nombre = find;
     },
+
     category: (state, action) => {
       const categoria = [...state.products[0]];
       const [find] = categoria.filter(
