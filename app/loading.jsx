6246a3a0b954c88135d150d2c3abf9a9ee48b5ab
EMelendezCamacho <eduardomelendezc5@gmail.com>
2023-07-08 10:00:24 -0600

    feat(app/loading):create loading.jsx

---
+++ app/loading.jsx
@@ -0,0 +1,6 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <div className="grid justify-center items-center w-[800px] h-[800px]">
+        <img src="https://i.pinimg.com/originals/ea/b7/e1/eab7e1120c9dd628d3bb39a20a94927d.gif" alt="" />
+    </div>
+  }
