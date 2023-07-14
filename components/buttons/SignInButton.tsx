4f2013dd7ec260603a6a6dcc96b24f5abc25b045
EMelendezCamacho <eduardomelendezc5@gmail.com>
2023-07-14 12:35:05 -0600

    fix(components/buttons/SignInButton):arreglar google

--- components/buttons/SignInButton.tsx
+++ components/buttons/SignInButton.tsx
@@ -5,18 +5,37 @@ import { useSession, signIn } from "next-auth/react";
 import logoGoogle from "../../public/images/google-logo.png";
 import Image from "next/image";
 
-const SignInButton = () => {
+const SignInButton = ({setForm}) => {
   const { data: session } = useSession();
+  
+
+  const handlerAuthSession = () => {
+    signIn()
+    if (session && session.user){
+      setForm({
+        email: session.user.email,
+        password: "cuenta google",
+        name: session.user.name,
+        image:session.user.image,
+        rol: "USER",
+      })
+    }
+    ;
+    
+    
+    return "eres una verga por suscribirte a artify"
+  }
 
   if (session && session.user) {
+  
     return null;
   }
-  return (
+   return (
     <div className="flex items-center justify-center">
       <button
         className="border border-black gap-5 flex justify-center items-center hover:bg-[var(--background-sec)] text-lg font-bold py-3 px-3 rounded-xl focus:outline-none focus:shadow-outline w-full"
         type="submit"
-        onClick={() => signIn()}>
+        onClick={handlerAuthSession}>
         <Image
           src={logoGoogle}
           width={20}
