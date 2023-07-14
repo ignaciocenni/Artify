73cdaa3394b7d562928241b732cf6a095b6007be
Ivo Serra <ivoozmarserra@gmail.com>
2023-07-14 18:32:00 -0300

    fix (components/CartComponents) fix local.storage;

--- components/CartComponent.jsx
+++ components/CartComponent.jsx
@@ -5,12 +5,16 @@ import BuyNowButton from "../components/buttons/BuyNowButton"
 import CardCart from "../app/cart/cardCart/CardCart"
 export default function CartComponent () {
     const [products, setProducts] = useState([])
+    //const [price, setPrice] = useState([])
     useEffect(() => {
       const cartProducts = JSON.parse(localStorage.getItem("products")) || [];
       setProducts(cartProducts)
+      
+      // const totalPrice = prices.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+      // setPrice(prices)
+      // console.log(price);
     }, [])
     
-
     return(
     <div className="flex mx-auto w-full md:w-3/5 lg:w-2/5 xl:w-1/3 items-start justify-center gap-10">
       <div className="flex flex-col p-4 bg-[var(--primary)] rounded-2xl shadow-md shadow-gray-600">
@@ -23,11 +27,12 @@ export default function CartComponent () {
             image={product.image}
             price={product.unit_price}
             stock={product.stock}
+            quantity={product.quantity}
           />
         ))}
       </div>
       <div className="flex flex-col p-4 bg-[var(--primary)] rounded-2xl shadow-md shadow-gray-600">
-        <h1 className="font-bold">Total $5334.3</h1>
+        <h1 className="font-bold">{products.price_multiplied}</h1>
       <BuyNowButton/>
       </div>
     </div>
