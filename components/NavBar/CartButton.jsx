2b18bdc18d5eaba6a4b47688db0c949376309dc5
Ivo Serra <ivoozmarserra@gmail.com>
2023-07-14 19:41:50 -0300

    fix(components/NavBar/CartButton) fix variables innecesarias

--- components/NavBar/CartButton.jsx
+++ components/NavBar/CartButton.jsx
@@ -1,21 +1,19 @@
 "use client";
 import Image from "next/image";
 import cart from "../../public/images/cart.svg";
+import { useSelector } from "react-redux";
 
 export default function CartButton({ products}) {
- 
-  const amount = products.map((product)=>{
-   return product.quantity
-  })
+ const quantity = useSelector((state) => state.valores.cartQuantity)
   
-  const totalAmount = amount.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+ 
   
   return (
     <div className="p-1 relative hover:bg-[var(--primary)] hover:shadow-lg rounded-full ">
       <Image src={cart} alt="cart" />
       {totalAmount > 0 && (
         <div className="absolute bottom-0 right-0 flex items-center justify-center w-6 h-6 bg-[var(--extra)] rounded-full border-2 border-black">
-          <span className="text-white text-xs">{totalAmount}</span>
+          <span className="text-white text-xs">{quantity}</span>
         </div>
       )}
     </div>
