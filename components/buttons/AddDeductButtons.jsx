d82a384e149368e2d00de727d35b23c96d6cd028
Ivo Serra <ivoozmarserra@gmail.com>
2023-07-14 00:57:06 -0300

    fix(AddDeductButtons)fix local.storage data;

--- components/buttons/AddDeductButtons.jsx
+++ components/buttons/AddDeductButtons.jsx
@@ -1,82 +1,77 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const AddDeductButtons = ({ data }) => {
   const product = {
     title: data.name,
     unit_price: data.price,
     quantity: 1,
+    stock: data.stock,
   };
-  const [currentStock, setCurrentStock] = useState(0);
-  const [arrProduct, setArrProduct] = useState([]);
-  console.log(product);
-  console.log(arrProduct);
-  // const handleAddProduct = () => {
-  //     if(data.stock <= currentStock){
-  //         product.quantity = product.quantity + currentStock
-  //         setCurrentStock(currentStock + 1);
-  //         setArrProduct([...arrProduct, product])
-  //         return updatedProduct;
-  //     }
-  //   };
-  const localStorageSave = () => {
-    try {
-      const existingData = JSON.parse(localStorage.getItem("products")) || [];
-      const productIndex = existingData.findIndex(
-        (p) => p.title === product.title
-      );
-      if (productIndex !== -1) {
-        existingData[productIndex] = product;
-      } else {
-        existingData.push(product);
-      }
-      localStorage.setItem("products", JSON.stringify(existingData));
-    } catch (error) {
-      console.error("Error al guardar los datos en localStorage:", error);
-      // Maneja el error según sea necesario (por ejemplo, muestra un mensaje de error al usuario)
-    }
-  };
-  const handleAddProduct = () => {
-    if (data.stock > currentStock) {
-      const updatedProduct = { ...product };
-      updatedProduct.quantity = updatedProduct.quantity + 1;
-  
-      const productIndex = arrProduct.findIndex(
-        (p) => p.title === updatedProduct.title
-      );
-      if (productIndex !== -1) {
-        const updatedArrProduct = [...arrProduct];
-        updatedArrProduct[productIndex] = updatedProduct;
-        setArrProduct(updatedArrProduct);
-      } else {
-        setArrProduct([...arrProduct, updatedProduct]);
-      }
-  
-      localStorageSave();
-    }
-  };
-  const handleDeductProduct = () => {
-    if (currentStock > 0) {
-      const updatedProduct = { ...product };
-      updatedProduct.quantity = updatedProduct.quantity - 1;
-  
-      const productIndex = arrProduct.findIndex(
-        (p) => p.title === updatedProduct.title
-      );
-      if (productIndex !== -1) {
-        const updatedArrProduct = [...arrProduct];
-        updatedArrProduct[productIndex] = updatedProduct;
-        setArrProduct(updatedArrProduct);
-      } else {
-        setArrProduct([...arrProduct, updatedProduct]);
-      }
-  
-      localStorageSave();
-    }
-  };
-  
+  const initializeState = () => {
+    const storedProducts = JSON.parse(localStorage.getItem("products")) || [];
+    const existingProduct = storedProducts.find(p => p.title === product.title);
 
+    if (existingProduct) {
+      return {
+        arrProduct: storedProducts,
+        currentQuantity: existingProduct.quantity,
+      };
+    } else {
+      return {
+        arrProduct: storedProducts,
+        currentQuantity: product.quantity,
+      };
+    }
+  };
+
+  const [arrProduct, setArrProduct] = useState([]);
+  const [currentQuantity, setCurrentQuantity] = useState(1);
+
+  useEffect(() => {
+    const { arrProduct, currentQuantity } = initializeState();
+    setArrProduct(arrProduct);
+    setCurrentQuantity(currentQuantity);
+  }, [product.title]);
+
+  const handleAddProduct = () => {
+    if (currentQuantity + 1 > product.stock) {
+      return; 
+    }
+
+    const existingProduct = arrProduct.find(p => p.title === product.title);
+
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+      setCurrentQuantity(existingProduct.quantity);
+      localStorage.setItem("products", JSON.stringify(arrProduct));
+    } else {
+      const updatedProduct = { ...product };
+      updatedProduct.quantity = currentQuantity + 1;
+      setArrProduct(prevArrProduct => [...prevArrProduct, updatedProduct]);
+      localStorage.setItem(
+        "products",
+        JSON.stringify([...arrProduct, updatedProduct])
+      );
+      setCurrentQuantity(updatedProduct.quantity);
+    }
+  };
+
+  const handleDeductProduct = () => {
+    if (currentQuantity > 1) {
+      setCurrentQuantity(prevQuantity => prevQuantity - 1);
+      const existingProduct = arrProduct.find(p => p.title === product.title);
+
+      if (existingProduct) {
+        existingProduct.quantity -= 1;
+        localStorage.setItem("products", JSON.stringify(arrProduct));
+      }
+    }
+  };
+  const handleClearProducts = () => {
+    localStorage.removeItem("products");
+    setArrProduct([]);
+    setCurrentQuantity(product.quantity);
+  };
   return (
     <div className="flex items-center">
       <button
@@ -85,13 +80,19 @@ const AddDeductButtons = ({ data }) => {
       >
         <h1 className="text-xs font-extrabold">Sumar producto</h1>
       </button>
-      <p>{product.quantity}</p>
-      <button 
-      onClick={handleDeductProduct}
-      className="hover:bg-[var(--background-sec)] hover:text-black w-[100%] text-white bg-[var(--detail)] py-5  rounded-lg flex content-center items-center gap-5 shadow-xl">
+      <p>Quantity: {currentQuantity}</p>
+      <p>Stock: {product.stock - currentQuantity}</p>
+      <button
+        onClick={handleDeductProduct}
+        className="hover:bg-[var(--background-sec)] hover:text-black w-[100%] text-white bg-[var(--detail)] py-5  rounded-lg flex content-center items-center gap-5 shadow-xl"
+      >
         <h1 className="text-xs font-extrabold">Restar producto</h1>
       </button>
+      <div>
+      <button onClick={handleClearProducts}>Deshacer Products</button>
+      </div>
     </div>
   );
 };
+
 export default AddDeductButtons;
