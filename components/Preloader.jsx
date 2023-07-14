fa55fc2bd5a8a49bc913662c98050b90932912a4
Ivo Serra <ivoozmarserra@gmail.com>
2023-07-14 19:41:05 -0300

    feat(components/Preloader) feat dispatch local.storage data;

--- components/Preloader.jsx
+++ components/Preloader.jsx
@@ -1,17 +1,31 @@
 "use client";
 import { useDispatch } from "react-redux";
-import { GET_PROVINCES, GET_CATEGORIES, GET_INFO } from "../store/slice";
-import { useRef } from "react";
+import { GET_PROVINCES, GET_CATEGORIES, GET_INFO, multiplied, totalPrices } from "../store/slice";
+import {useRef } from "react";
 
 export default function Preloader({ data }) {
-  const { categories, provinces, products } = data;
+  const { categories, provinces, AllProducts } = data;
   const dispatch = useDispatch();
   const loaded = useRef(false);
   if (!loaded.current) {
     dispatch(GET_CATEGORIES(categories));
     dispatch(GET_PROVINCES(provinces));
-    dispatch(GET_INFO(products));
+    dispatch(GET_INFO(AllProducts));
     loaded.current = true;
   }
+
+      const products = localStorage.getItem("products");
+      console.log(products);
+      const totalPrice = products.map((product) =>{
+        return product.quantity * product.unit_price
+      })
+      const quantityProducts= products.map((product) =>{
+        return product.quantity
+      })
+      dispatch(multiplied(quantityProducts))
+      dispatch(totalPrices(totalPrice))
+  
+
+
   return null;
 }
