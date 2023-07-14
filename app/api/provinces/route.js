37620d2c1d9e2ead351b4160fbe228a707247f92
Tomás <t.diaz.soto@gmail.com>
2023-07-13 20:59:46 -0400

    fix(api/products route.js): fix imports

--- app/api/provinces/route.js
+++ app/api/provinces/route.js
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
-import { allProvinces } from "./controllers";
+import { getAllProvinces } from "./controllers";
 
 export async function GET() {
   try {
-    const response = await allProvinces();
+    const response = await getAllProvinces();
     return NextResponse.json(response, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 404 });
