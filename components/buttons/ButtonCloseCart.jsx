f20435e02057593ce0a5bb105f260684c8aa38c5
H3rC4 <hernanenriquecaballero@gmail.com>
2023-07-15 20:20:07 +0200

    add()compponent/buttons/ButtoncloseCart.jsx)

---
+++ components/buttons/ButtonCloseCart.jsx
@@ -0,0 +1,17 @@
+
+export default function ButtonCloseCart({id,setProducts}) {
+  const cartList = JSON.parse(localStorage.getItem("products"));
+  const handlerClick = (id)=>{
+  const newCartList = cartList.filter(item=>item.id != id)
+  localStorage.setItem("products", JSON.stringify(newCartList));
+  setProducts(newCartList)
+  console.log(JSON.parse(localStorage.getItem("products")))
+}
+ 
+    return (
+    <button onClick={()=> handlerClick(id)}
+    className="px-3  py-1 rounded-r-lg hover:bg-[var(--background-sec)] hover:text-black text-white font-bold bg-[var(--detail)]  flex content-center items-center shadow-xl transition-colors ">
+      X
+    </button>
+  )
+}
