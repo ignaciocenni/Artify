fc8112089833f85f9742fd6572522a6c2dbc8827
EMelendezCamacho <eduardomelendezc5@gmail.com>
2023-07-16 15:53:06 -0600

    add(email notification):se pasan datos para el correo

--- components/PurchaseStatusComponent.jsx
+++ components/PurchaseStatusComponent.jsx
@@ -2,14 +2,25 @@
 import { useSession } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 
-export default function PurchaseStatusComponent () {
-    const { data: session } = useSession();
+export default function PurchaseStatusComponent() {
+    const data = useSession();
 
     const searchParams = useSearchParams()
     const status = searchParams.get('status')
-    
-    
-    return(
+
+    const form = {
+        email: data.data.user.email,
+        name: data.data.user.name,
+        satatus: status
+    }
+    const sendEmail = async () => {
+        await sendContactForm(form);
+    };
+    sendEmail()
+
+
+
+    return (
         <div>
             {/* {session && session.user ? null :} */}
         </div>
