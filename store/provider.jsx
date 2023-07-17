f56ccb004aa90471c954816d17efba73fc8d410f
ignaciocenni <ignaziocenni@gmail.com>
2023-07-17 14:29:50 -0300

    fix(/store Provider.jsx): change div in return for fragments

--- store/provider.jsx
+++ store/provider.jsx
@@ -1,10 +1,11 @@
-'use client'
+"use client";
 import { Provider } from "react-redux";
-import store from './store'
+import store from "./store";
 
-const Providers = ({children})=>{
-    return <Provider store={store}>
-        {children}
-    </Provider>
+export default function Providers({ children }) {
+  return (
+    <>
+      <Provider store={store}>{children}</Provider>
+    </>
+  );
 }
-export default Providers;
