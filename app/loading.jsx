fced07b1c053e615dc3d966432ddc56ce9a9072e
ignaciocenni <ignaziocenni@gmail.com>
2023-07-17 14:56:23 -0300

    fix(/ loading.jsx /components CartButton.jsx): add fragments

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <div className="flex flex-col justify-center items-center">
-        <Image src="https://i.pinimg.com/originals/ea/b7/e1/eab7e1120c9dd628d3bb39a20a94927d.gif" alt="cargando.." width={400} height={400} />
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <Image
+          src="https://i.pinimg.com/originals/ea/b7/e1/eab7e1120c9dd628d3bb39a20a94927d.gif"
+          alt="animacion de carga"
+          width={400}
+          height={400}
+        />
         <h1 className="font-bold text-xl">Cargando...</h1>
-    </div>
-  }
+      </div>
+    </>
+  );
+}
