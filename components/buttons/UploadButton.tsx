278c5eb60a4aebb3dd6a4afabe34f66fd5983237
Tomás <t.diaz.soto@gmail.com>
2023-07-18 14:22:08 -0400

    fix(components/buttons/ UploadButton): fix imports

--- components/buttons/UploadButton.tsx
+++ components/buttons/UploadButton.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-// You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
 import "@uploadthing/react/styles.css";
-import "../../node_modules/@uploadthing/react/style.css";
 
 import { OurFileRouter } from "../../app/api/uploadthing/core";
 import { UploadButton } from "@uploadthing/react";
@@ -16,26 +14,13 @@ export default function UploadButtonPage(props) {
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
           if (res) {
-          //   setForm({
-          //     ...form,
-          //     image: res.map((img) => img.fileUrl
-          //     ),
-          //   });
-          // }
-
-
-          //TODO: MODULARIZAR TODO EL CODIGO BY NAC-HITLER-EL-MAS-LINDO
-          //TODO: CAMBIAR CUANDO LA BASE DE DATOS PUEDA RECIBIR MAS DE UNA IMAGEN
-
-          setForm({
-            ...form,
-            image: res[0].fileUrl
-            
-          });
-        }
+            setForm({
+              ...form,
+              image: res[0].fileUrl,
+            });
+          }
         }}
         onUploadError={(error: Error) => {
-          // Do something with the error.
           alert(`ERROR! ${error.message}`);
         }}
       />
