f15ef06b1ba069d4dc94b9ebbece164549187291
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-07-17 21:11:56 -0300

    fix(codicional chaining)

--- components/DetailComponents/CategoryLabel.jsx
+++ components/DetailComponents/CategoryLabel.jsx
@@ -4,7 +4,7 @@ function CategoryLabel({ category }) {
   return (
     <div className="flex flex-wrap items-center content-center gap-1">
       <div className="flex py-1 px-5 items-center content-center gap-2 rounded-2xl bg-[var(--background-sec)] text-center font-semibold text-base">
-        {category.name.charAt(0).toUpperCase() + category.name.slice(1)}
+        {category?.name.charAt(0).toUpperCase() + category?.name.slice(1)}
       </div>
     </div>
   );
