de74a3f3d69fdecf7a886583b662162e7cc3a4fb
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-07-19 23:19:09 -0300

    feat(dash&settings) new layout& groped folders

---
+++ app/(dashsettings)/settings/page.jsx
@@ -0,0 +1,9 @@
+import FormML from "../../../components/SettingsComponents/formML";
+
+export default function Settings() {
+  return (
+    <section className="flex w-full">
+      <FormML />
+    </section>
+  );
+}
