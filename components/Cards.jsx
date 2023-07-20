50bfaecffd9f7641497da2b90c7f345670378468
EnzoGabrielSb <enzogabrielvera07@gmail.com>
2023-07-20 17:27:13 -0300

    fix:createComponentNotFound

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,13 +1,14 @@
 "use client";
 import Card from "./Card";
 import Loading from "../app/loading";
+import NotFound from "../app/notFound";
 
-export default function Cards({ products, error }) {
+export default function Cards({ products }) {
   const response = products;
 
   return (
     <div className="grid grid-cols-4 w-1/2 px-2">
-      {response.length ? (
+      {response.length > 0 ? (
         response.map((resp) => (
           <Card
             key={resp.id}
@@ -19,8 +20,8 @@ export default function Cards({ products, error }) {
             userImage={resp.user.image}
           />
         ))
-      ) : response.error ? (
-        "no se encontraron resultados"
+      ) : response.length === 0 ? (
+        <NotFound />
       ) : (
         <Loading />
       )}
