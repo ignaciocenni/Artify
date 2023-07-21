4d27697535a147b719bc7fac57dba4f25d204f81
Sofia Ibañez <112526595+SofiaGIb@users.noreply.github.com>
2023-07-20 23:23:26 -0300

    feat(buttons):add condicional

--- components/buttons/FbButton.jsx
+++ components/buttons/FbButton.jsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
-import icono from "../../public/images/facebookicons.svg"
- function FbButton({socials}) {
+import icono from "../../public/images/facebookicons.svg";
+import iconoGrey from "../../public/images/facebookgrey.svg";
+function FbButton({ socials }) {
   return (
     <>
-      <a href="https://www.facebook.com/">
-        <button>
-          <Image alt="icono" src={icono} width={40} height={40} />
+      {socials ? (
+        <a href="https://www.facebook.com/">
+          <button>
+            <Image alt="icono" src={icono} width={40} height={40} />
+          </button>
+        </a>
+      ) : (
+        <button className="cursor-not-allowed">
+          <Image alt="icono" src={iconoGrey} width={40} height={40} />
         </button>
-      </a>
+      )}
     </>
   );
 }
-export default FbButton
+export default FbButton;
--- components/buttons/IgButton.jsx
+++ components/buttons/IgButton.jsx
@@ -1,18 +1,22 @@
-import Image from "next/image"
-import icono from "../../public/images/instagramicons.svg"
+import Image from "next/image";
+import icono from "../../public/images/instagramicons.svg";
+import iconoGrey from "../../public/images/instagramgrey.svg";
 
-
-function IgButton({socials}) {
-    
+function IgButton({ socials }) {
   return (
     <>
-    <a href="https://www.instagram.com/">
-
-     <button>
-        <Image alt="icono" src={icono} width={40} height={40} />
-        </button> 
-    </a>
+      {socials ? (
+        <a href="https://www.instagram.com/">
+          <button>
+            <Image alt="icono" src={icono} width={40} height={40} />
+          </button>
+        </a>
+      ) : (
+        <button className="cursor-not-allowed">
+          <Image alt="icono" src={iconoGrey} width={40} height={40} />
+        </button>
+      )}
     </>
-  )
+  );
 }
-export default IgButton
+export default IgButton;
