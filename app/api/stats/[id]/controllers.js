d8af928ad1e4a725365240daade16c2067aecc1b
Tomás <t.diaz.soto@gmail.com>
2023-07-21 10:05:57 -0400

    reafactor(api/stats route controllers ): información separada para admin y user

--- app/api/stats/[id]/controllers.js
+++ app/api/stats/[id]/controllers.js
@@ -1,52 +1,5 @@
 import prisma from "../../db/client";
 
-// ADMIN
-const getTotalProducts = async () => {
-  const products = await prisma.product.findMany();
-
-  let productStatus = {
-    ACTIVE: 0,
-    INACTIVE: 0,
-    PENDING: 0,
-  };
-
-  products.map((product) => {
-    if (product.status === "ACTIVE") productStatus.ACTIVE++;
-    if (product.status === "INACTIVE") productStatus.INACTIVE++;
-    if (product.status === "PENDING") productStatus.PENDING++;
-  });
-
-  return productStatus;
-};
-
-const getProvinceSales = async () => {
-  const sales = await prisma.sales.findMany();
-  const provinces = await prisma.province.findMany();
-  const products = await prisma.product.findMany();
-
-  let provinceSales = {};
-
-  provinces.map((province) => {
-    provinceSales[province.name] = 0;
-  });
-
-  sales.map((sale) => {
-    products.map(async (product) => {
-      if (product.id === sale.productId) {
-        let province = await prisma.province.findFirst({ where: { id: product.provinceId } });
-        provinceSales[province.name]++;
-      }
-    });
-  });
-  return provinceSales;
-};
-
-const getTotalUsers = async () => {
-  const users = await prisma.user.findMany();
-
-  return users.length;
-};
-
 // USER
 const getUserSales = async (id) => {
   const user = await prisma.user.findFirst({
@@ -110,4 +63,4 @@ const getUserProvinceSales = async (id) => {
   return userProvinceSales;
 };
 
-export { getUserSales, getUserProducts, getUserProvinceSales, getTotalProducts, getProvinceSales, getTotalUsers };
+export { getUserSales, getUserProducts, getUserProvinceSales };
--- app/api/stats/[id]/route.js
+++ app/api/stats/[id]/route.js
@@ -1,23 +1,19 @@
 import { NextResponse } from "next/server";
-import { getUserSales, getUserProducts, getUserProvinceSales, getTotalProducts, getProvinceSales, getTotalUsers } from "./controllers";
+import { getUserSales, getUserProducts, getUserProvinceSales } from "./controllers";
 
 export async function GET(req, { params }) {
   const { id } = params;
 
   try {
-    // ADMIN
-    const totalUsers = await getTotalUsers(); //total de usuarios
-    const provinceSales = await getProvinceSales(); //total de ventas por provincia
-    const productStatus = await getTotalProducts(); //total de productos segun su estado
-
     // USER
     const totalSales = await getUserSales(id); //total de ventas de un usuario
     const productUserStatus = await getUserProducts(id); //total de productos segun su estado de un usuario
     const userProvinceSales = await getUserProvinceSales(id); //total de ventas por provincia de un usuario
     return NextResponse.json(
       {
-        admin: { totalUsers: totalUsers, provinceSales: provinceSales, productStatus: productStatus },
-        user: { totalSales: totalSales, productUserStatus: productUserStatus, userProvinceSales: userProvinceSales },
+        totalSales: totalSales,
+        productUserStatus: productUserStatus,
+        userProvinceSales: userProvinceSales,
       },
       { status: 200 }
     );
