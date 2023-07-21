1f735f85f50f8bbfde28f705c9440229b7722897
Tomás <t.diaz.soto@gmail.com>
2023-07-21 19:06:36 -0400

    fix(app/profile/[userId] page): delete localhost fetching

--- app/profile/[userId]/page.jsx
+++ app/profile/[userId]/page.jsx
@@ -1,18 +1,18 @@
-'use client'
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Cards from '../../../components/Cards';
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cards from "../../../components/Cards";
+import Image from "next/image";
+import Link from "next/link";
 
 const getUser = async (id) => {
-  const { data } = await axios.get(`http://localhost:3000/api/users/${id}`);
+  const { data } = await axios.get(`/api/users/${id}`);
   return data;
 };
 
 const getProducts = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/products');
-  console.log(data)
+  const { data } = await axios.get("/api/products");
+  console.log(data);
   return data;
 };
 
@@ -33,7 +33,7 @@ export default function Profile({ params }) {
     fetchData();
   }, [userId]);
 
-   const userProducts = products && products.filter((product) => product.user.name === userData.name);
+  const userProducts = products && products.filter((product) => product.user.name === userData.name);
 
   return (
     <div className="z-10">
@@ -42,15 +42,14 @@ export default function Profile({ params }) {
           <div className="flex w-2/4 justify-evenly">
             <div className="flex justify-center flex-col items-center">
               <Image src={userData.image} width={144} height={144} alt="me" />
-              <h1 className="pb-2 font-bold text-3xl">{userData.name} {userData.lastName}</h1>
+              <h1 className="pb-2 font-bold text-3xl">
+                {userData.name} {userData.lastName}
+              </h1>
               <p className="pb-2">Artesano</p>
-              <div className="flex w-full justify-around">
-              </div>
+              <div className="flex w-full justify-around"></div>
             </div>
             <div className="flex w-2/4 items-center">
-              <p className="text-sm">
-              {userData && userData.aboutMe}
-              </p>
+              <p className="text-sm">{userData && userData.aboutMe}</p>
             </div>
           </div>
         </div>
