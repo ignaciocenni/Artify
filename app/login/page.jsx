9939d5972ed526cfbefa7885cd4f8e275ea0bc96
ignaciocenni <ignaziocenni@gmail.com>
2023-07-21 11:58:57 -0300

    fix(/login page.jsx): add preventDefault()

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -8,7 +8,6 @@ import InputField from "../../components/inputs/InputField";
 import { validate } from "../signin/validate";
 
 export default function LoginPage({ searchParams }) {
-
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -34,7 +33,8 @@ export default function LoginPage({ searchParams }) {
     }
   }, [searchParams]);
 
-  const onSubmit = async () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
     const { email, password } = form;
     await signIn("credentials", { email, password });
   };
@@ -43,7 +43,7 @@ export default function LoginPage({ searchParams }) {
     const name = event.target.name;
     const value = event.target.value;
     setForm({ ...form, [name]: value });
-    setErrors(validate({ ...form, [name]: value }));;
+    setErrors(validate({ ...form, [name]: value }));
     setCredentialError("");
   };
 
@@ -53,45 +53,43 @@ export default function LoginPage({ searchParams }) {
         <form className="w-96 flex flex-col gap-5" onSubmit={onSubmit}>
           <h1 className="font-semibold text-3xl mb-5">Iniciar Sesión</h1>
           <div className="mb-4">
-            <InputField 
-            id="username"
-            type="text"
-            placeholder="Correo electrónico"
-            onChange={handleChange}
-            name="email"
-            value={form.email}
-            errors={errors.email}
-            />           
+            <InputField
+              id="username"
+              type="text"
+              placeholder="Correo electrónico"
+              onChange={handleChange}
+              name="email"
+              value={form.email}
+              errors={errors.email}
+            />
           </div>
-          
+
           <div className="mb-6">
-          <InputField 
-            id="password"
-            type="password"
-            placeholder="Contraseña"
-            onChange={handleChange}
-            name="password"
-            value={form.password}
-            errors={errors.password}
-            />           
+            <InputField
+              id="password"
+              type="password"
+              placeholder="Contraseña"
+              onChange={handleChange}
+              name="password"
+              value={form.password}
+              errors={errors.password}
+            />
           </div>
           {credentialsError && <p className=" text-red-700">{credentialsError}</p>}
           <div className="flex items-center justify-center">
-            <button
-              className="bg-[var(--detail)] hover:bg-[var(--background-sec)] hover:text-black text-lg  text-white font-bold py-3 px-3 rounded-xl focus:outline-none focus:shadow-outline w-full"
-            >
+            <button className="bg-[var(--detail)] hover:bg-[var(--background-sec)] hover:text-black text-lg  text-white font-bold py-3 px-3 rounded-xl focus:outline-none focus:shadow-outline w-full">
               Continuar
             </button>
           </div>
 
-          <a className="inline-block align-baseline font-bold text-sm text-slate-500  hover:text-[var(--background-sec)] mt-2" href="#">Olvido su contraseña?
+          <a className="inline-block align-baseline font-bold text-sm text-slate-500  hover:text-[var(--background-sec)] mt-2" href="#">
+            Olvido su contraseña?
           </a>
           <hr />
           <SignInButton />
           <Link href="/signin">
-            <button
-              className="bg-[var(--detail)] hover:bg-[var(--background-sec)] hover:text-black text-lg  text-white font-bold py-3 px-3 rounded-xl focus:outline-none focus:shadow-outline w-full"              
-            >Cuenta Nueva
+            <button className="bg-[var(--detail)] hover:bg-[var(--background-sec)] hover:text-black text-lg  text-white font-bold py-3 px-3 rounded-xl focus:outline-none focus:shadow-outline w-full">
+              Cuenta Nueva
             </button>
           </Link>
         </form>
