6df93fcc17460494afa3b241d1437052e26ea06d
H3rC4 <hernanenriquecaballero@gmail.com>
2023-07-21 13:00:06 +0200

    fix(component/Cards.jsx): fix bugs userId

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -3,12 +3,11 @@ import Card from "./Card";
 import Loading from "../app/loading";
 import NotFound from "../app/notFound";
 
-export default function Cards({ products }) {
-  const response = products;
-
+export default  function Cards({ products }) {
+  const response =  products;
   return (
     <div className="grid grid-cols-4 w-1/2 px-2">
-      {response.length > 0 ? (
+      {response && response.length > 0 ? (
         response.map((resp) => (
           <Card
             key={resp.id}
@@ -18,9 +17,10 @@ export default function Cards({ products }) {
             price={resp.price}
             title={resp.name}
             userImage={resp.user.image}
+            userId={resp.userId}
           />
-        ))
-      ) : response.length === 0 ? (
+        )) 
+      ) : response && response.length ? (
         <NotFound />
       ) : (
         <Loading />
