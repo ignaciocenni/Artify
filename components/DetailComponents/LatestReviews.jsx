11275a5f91fb2438d5b6ff8c07a7ff9b1bb2c2c4
H3rC4 <hernanenriquecaballero@gmail.com>
2023-07-25 01:40:29 +0200

    refactor(DetailContent/latestReviews)

--- components/DetailComponents/LatestReviews.jsx
+++ components/DetailComponents/LatestReviews.jsx
@@ -1,16 +1,25 @@
+import Stars from "../Stars";
 function LatestReviews({ reviews, latestReview }) {
   return (
     <div>
       <h1 className="font-semibold">Últimos realizados</h1>
       {reviews?.map((rev) => (
-        <div key={rev.id}>
-          <p>{rev.comment}</p>
+        <div key={rev.id} className='flex flex-col  pb-4 border-zinc-300 border-b-2'>
+          <div className="flex justify-between items-center mb-3">
+            <p ><Stars averange={rev.rating} size={20} /></p>
+            <p >2024-12-12</p>
+          </div>
+          <p className="text-zinc-700">{rev.comment}</p>
         </div>
       ))}
       {latestReview && (
-        <div>
-          <p>{latestReview.comment}</p>
+        <div key={latestReview.id} className='flex flex-col  pb-4 border-zinc-300 border-b-2'>
+        <div className="flex justify-between items-center mb-3">
+          <p ><Stars averange={latestReview.rating} size={20} /></p>
+          <p >creado recientemente</p>
         </div>
+        <p className="text-zinc-700">{latestReview.comment}</p>
+      </div>
       )}
     </div>
   );
