3531fde1494f7f9c64e9b54f3d9a95eda9f13106
Ignacio Cenni <ignaziocenni@gmail.com>
2023-07-24 20:41:38 -0300

    refact(/components notFound.jsx): change tags HTML

--- components/notFound.jsx
+++ components/notFound.jsx
@@ -3,17 +3,14 @@ import image from "../public/images/search-not-found.png";
 
 export default function notFound() {
   return (
-    <>
-      <div className="w-full h-[80vh] flex flex-col  justify-center items-center" >
-        <Image src={image} width={200} height={200} alt="notfound" />
-
-        <h1 className="font-semibold text-2xl mt-6">
-          No hay publicaciones que coincidan con tu búsqueda
-        </h1>
-        <h1 className="text-zinc-600">
-        Haz una nueva búsqueda. Comprueba la ortografía, cambia los filtros o busca un término menos específico.
-        </h1>
-      </div>
-    </>
+    <section className="w-full h-[80vh] flex flex-col  justify-center items-center">
+      <Image src={image} width={200} height={200} alt="notfound" />
+      <article className="text-center mx-auto w-11/12 py-4">
+        <p className="font-semibold text-2xl">No hay publicaciones que coincidan con tu búsqueda</p>
+        <p className="text-zinc-600 mt-2">
+          Haz una nueva búsqueda. Comprueba la ortografía, cambia los filtros o busca un término menos específico.
+        </p>
+      </article>
+    </section>
   );
 }
