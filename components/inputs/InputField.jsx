288612b60eb72a80e7a418601fd5c0ebdba96850
Sofia Ibañez <112526595+SofiaGIb@users.noreply.github.com>
2023-07-25 18:51:46 -0300

    feat(app/inputField):restyle

--- components/inputs/InputField.jsx
+++ components/inputs/InputField.jsx
@@ -2,7 +2,7 @@ const InputField = ({ id, type, placeholder, onChange, name, value, errors }) =>
   return (
     <div className="mb-4">
       <input
-        className="flex gap-3 font-bold text-3xl shadow appearance-none  rounded-xl w-full py-2 px-3 focus:outline-none focus:shadow-outline"
+        className="flex gap-3 bg-[var(--primary)] text-lg shadow appearance-none  rounded-xl w-full py-2 px-3 focus:outline-none focus:shadow-outline"
         id={id}
         type={type}
         placeholder={placeholder}
