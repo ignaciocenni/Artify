aaa5a8409ca356129f47a87644b1f51d1b5151aa
Tomás <t.diaz.soto@gmail.com>
2023-07-25 09:56:41 -0400

    fix(components/dashboard): fix async problem

--- components/dashboard/CardsPublication.jsx
+++ components/dashboard/CardsPublication.jsx
@@ -1,16 +1,25 @@
 "use client";
 import CardPublication from "./CardPublication";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { GET_INFO } from "../../store/slice";
+import axios from "axios";
 const CardsPublication = () => {
   const { data: session } = useSession();
-  const allproducts = useSelector((state) => state.valores.dashProducts);
+  const dispatch = useDispatch();
 
-  let products;
+  useEffect(() => {
+    const getProducts = () => {
+      return axios.get("/api/products");
+    };
+    const products = getProducts().then(() => (products.data ? dispatch(GET_INFO(products.data)) : ""));
+  }, [dispatch]);
+
+  let allproducts = useSelector((state) => state.valores.dashProducts);
+  let products = [...allproducts];
   if (session && session.user.role === "USER") {
-    products = allproducts.filter(
-      (product) => product.user.email === session.user.email
-    );
+    products = allproducts.filter((product) => product.user.email === session.user.email);
   } else {
     products = allproducts;
   }
@@ -32,9 +41,7 @@ const CardsPublication = () => {
           />
         ))
       ) : (
-        <div className="px-4 py-3 rounded-lg shadow-md items-center gap-3 inline-flex mx-3 font-medium">
-          No hay publicaciones
-        </div>
+        <div className="px-4 py-3 rounded-lg shadow-md items-center gap-3 inline-flex mx-3 font-medium">No hay publicaciones</div>
       )}
     </>
   );
