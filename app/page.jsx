1bb0cbbb707baf0f362db17bc6a33b1cfb0dc7b6
tod1az <t.diaz.soto@gmail.com>
2023-12-02 19:59:00 -0300

    refactor(page.jsx): server component

--- app/page.jsx
+++ app/page.jsx
@@ -1,36 +1,13 @@
-"use client";
-import Filters from "../components/Filters";
-import Cards from "../components/Cards";
-import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import LoadingPublication from "../components/loadings/LoadingPublication";
+import Filters from '../components/Filters'
+import Cards from '../components/Cards'
 
-export default function Home() {
-  const [products, setProducts] = useState(null);
-
-  let response = useSelector((state) => state.valores.activeProducts);
-
-  useEffect(() => {
-    setProducts(response);
-  }, [products, response]);
-
+export default async function Home() {
   return (
     <>
-      <div className="flex py-2 px-5 justify-center">
-        <div className="flex flex-col gap-6 w-auto mr-6 ">
-          <Filters />
-        </div>
-        {products ? (
-          <div className="flex justify-center items-center w-2/3 mt-[10vh]">
-            <Cards products={products} />
-          </div>
-        ) : (
-          <div className="flex flex-col justify-center items-center w-2/3 mt-[10vh]">
-            <LoadingPublication />
-            <LoadingPublication />
-          </div>
-        )}
+      <div className="flex py-2 px-5 justify-start">
+        <Filters />
+        <Cards />
       </div>
     </>
-  );
+  )
 }
