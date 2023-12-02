d0180410bd2877c995a7f1113c288af088ebae8d
tod1az <t.diaz.soto@gmail.com>
2023-12-02 20:00:08 -0300

    refactor(/components/cards & card): server component

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default function Card(props) {
-  const { id, image, user, price, title, userImage, userId } = props;
+export default function Card({ id, image, user, price, title, userImage, userId }) {
   return (
-    <div className="object-cover flex flex-col justify-center align-first grid-cols-1 pb-3 bg-[var(--primary)] rounded-3xl  shadow-sm shadow-zinc-400 w-full">
+    <div className="w-[200px] text-left flex flex-col justify-center align-first grid-cols-1 pb-3 bg-[var(--primary)] rounded-3xl  shadow-sm shadow-zinc-400 ">
       <div className="relative h-80">
         <Link href={`/detail/${id}`}>
           <Image
@@ -14,28 +13,22 @@ export default function Card(props) {
             fill
             sizes="100vw"
             style={{
-              objectFit: "cover",
+              objectFit: 'cover'
             }}
-            className="rounded-3xl"
+            className="rounded-t-3xl"
           />
         </Link>
       </div>
-      <div className="px-2">
-        <h3 className="text-sm font-medium pt-1">${price}</h3>
-        <h3 className="text-sm font-semibold py-2 w-full h-16">{title}</h3>
-        <div className="flex pt-1">
-          <Image
-            className="rounded-full"
-            src={userImage}
-            width={20}
-            height={20}
-            alt={`Imagen de perfil del usuario ${id}`}
-          />
+      <div className=" flex flex-col p-2 gap-2 ">
+        <h3 className="text-sm font-medium ">${price}</h3>
+        <h3 className="text-sm font-semibold  w-full  truncate">{title}</h3>
+        <div className="flex">
+          <Image className="rounded-full" src={userImage} width={20} height={20} alt={`Imagen de perfil del usuario ${id}`} />
           <Link href={`/profile/${userId}`}>
             <h3 className="px-1 text-sm font-light">{user}</h3>
           </Link>
         </div>
       </div>
     </div>
-  );
+  )
 }
--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,35 +1,35 @@
-"use client";
-import Card from "./Card";
-import LoadingPublication from "./loadings/LoadingPublication";
-import NotFounded from "./NotFounded";
+import Card from './Card'
+import LoadingPublication from './loadings/LoadingPublication'
+import NotFounded from './NotFounded'
+import { getActiveProducts } from '../app/lib/services'
 
-export default function Cards({ products }) {
+export default async function Cards() {
+  const products = await getActiveProducts()
+
+  if (!products) {
+    return <LoadingPublication />
+  }
+
+  if (products.length === 0) {
+    return <NotFounded />
+  }
+
   return (
     <>
-      {products && products.length > 0 ? (
-        <div className="grid grid-cols-5 px-2 gap-4 w-full items-center justify-center">
-          {products.map((resp) => (
-            <Card
-              key={resp.id}
-              id={resp.id}
-              image={resp.image}
-              user={`${resp.user.name} ${resp.user.lastName}`}
-              price={resp.price}
-              title={resp.name}
-              userImage={resp.user.image}
-              userId={resp.userId}
-            />
-          ))}
-        </div>
-      ) : products && products.length == 0 ? (
-        <div className="flex justify-center items-center w-full">
-          <NotFounded />
-        </div>
-      ) : (
-        <div className="flex flex-col justify-center items-center w-full">
-          <LoadingPublication />
-        </div>
-      )}
+      <div className="flex flex-wrap px-2 gap-4  items-center justify-center w-2/3 mt-[15vh]">
+        {products.map((resp) => (
+          <Card
+            key={resp.id}
+            id={resp.id}
+            image={resp.image}
+            user={`${resp.user.name} ${resp.user.lastName}`}
+            price={resp.price}
+            title={resp.name}
+            userImage={resp.user.image}
+            userId={resp.userId}
+          />
+        ))}
+      </div>
     </>
-  );
+  )
 }
