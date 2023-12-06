0dc095aa306a2ae666cf4181a988586e95f111ec
tod1az <t.diaz.soto@gmail.com>
2023-12-06 19:44:19 -0300

    refactor cards component

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Filters from '../components/Filters'
 import Cards from '../components/Cards'
+import { getActiveProducts } from './lib/services/products'
 
-export default async function Home() {
+export default async function Home({ searchParams }) {
+  const products = await getActiveProducts(searchParams)
   return (
     <>
-      <div className="flex py-2 px-5 justify-start">
+      <div className="flex py-2 px-5 justify-start mt-[15vh]">
         <Filters />
-        <Cards />
+        <Cards products={products} />
       </div>
     </>
   )
--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,35 +1,31 @@
 import Card from './Card'
 import LoadingPublication from './loadings/LoadingPublication'
 import NotFounded from './NotFounded'
-import { getActiveProducts } from '../app/lib/services'
+import { Suspense } from 'react'
 
-export default async function Cards() {
-  const products = await getActiveProducts()
-
-  if (!products) {
-    return <LoadingPublication />
-  }
-
+export default async function Cards({ products }) {
   if (products.length === 0) {
     return <NotFounded />
   }
 
   return (
     <>
-      <div className="flex flex-wrap px-2 gap-4  items-center justify-center w-2/3 mt-[15vh]">
-        {products.map((resp) => (
-          <Card
-            key={resp.id}
-            id={resp.id}
-            image={resp.image}
-            user={`${resp.user.name} ${resp.user.lastName}`}
-            price={resp.price}
-            title={resp.name}
-            userImage={resp.user.image}
-            userId={resp.userId}
-          />
-        ))}
-      </div>
+      <Suspense fallback={<LoadingPublication />}>
+        <div className="flex flex-wrap px-2 gap-4  items-center justify-center w-2/3">
+          {products.map((resp) => (
+            <Card
+              key={resp.id}
+              id={resp.id}
+              image={resp.image}
+              user={`${resp.user.name} ${resp.user.lastName}`}
+              price={resp.price}
+              title={resp.name}
+              userImage={resp.user.image}
+              userId={resp.userId}
+            />
+          ))}
+        </div>
+      </Suspense>
     </>
   )
 }
