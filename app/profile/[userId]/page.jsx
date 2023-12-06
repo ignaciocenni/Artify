80221f7f3341b18813b67b6b7a1cb3a4c740d78b
tod1az <t.diaz.soto@gmail.com>
2023-12-06 19:44:54 -0300

    refactor profile component rsc

--- app/profile/[userId]/page.jsx
+++ app/profile/[userId]/page.jsx
@@ -1,86 +1,43 @@
-"use client";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Image from "next/image";
-import Cards from "../../../components/Cards";
-import Footer from "../../../components/Footer";
-import WpButton from "../../../components/buttons/WpButton";
-import NotFounded from "../../../components/NotFounded";
-import LoadingProfile from "../../../components/loadings/LoadingProfile";
+import Image from 'next/image'
+import Cards from '../../../components/Cards'
+import Footer from '../../../components/Footer'
+import WpButton from '../../../components/buttons/WpButton'
+import NotFounded from '../../../components/NotFounded'
+import LoadingProfile from '../../../components/loadings/LoadingProfile'
+import { getUserInfo, getUserProducts } from '../../lib/services/users'
 
-const getUser = async (id) => {
-  const { data } = await axios.get(`/api/users/${id}`);
-  return data;
-};
-
-const getProducts = async () => {
-  const { data } = await axios.get("/api/products");
-  return data;
-};
-
-export default function Profile({ params }) {
-  const { userId } = params;
-  const [userData, setUserData] = useState(null);
-  const [products, setProducts] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const user = await getUser(userId);
-      setUserData(user);
-
-      const allProducts = await getProducts();
-      setProducts(allProducts);
-    };
-
-    fetchData();
-  }, [userId]);
-
-  const userProducts =
-    products &&
-    products.filter(
-      (product) =>
-        product.user.name === userData.name && product.status === "ACTIVE"
-    );
+export default async function Profile({ params }) {
+  const { userId } = params
+  const userInfo = await getUserInfo(userId)
+  const userProducts = await getUserProducts(userId)
 
   return (
     <>
       <div className="flex h-screen flex-col items-center pb-3 mt-[10vh] bg-[var(--background)] ">
-        {userData ? (
+        {userInfo ? (
           <div className="flex items-center flex-col w-1/2 mt-5">
             <div className="flex flex-col w-full items-center">
               <div className="flex justify-center flex-col items-center pt-5 pb-2 gap-5">
-                <Image
-                  src={userData.image}
-                  width={100}
-                  height={100}
-                  alt="me"
-                  className="rounded-full shadow-md shadow-gray-500"
-                />
+                <Image src={userInfo.image} width={100} height={100} alt="me" className="rounded-full shadow-md shadow-gray-500" />
                 <h1 className="font-bold text-2xl">
-                  {userData.name} {userData.lastName}
-                  <WpButton socials={userData && userData.socials} />
+                  {userInfo.name} {userInfo.lastName}
+                  <WpButton socials={userInfo && userInfo.socials} />
                 </h1>
               </div>
               <div className="flex justify-between p-6 w-full">
                 <div className="flex flex-col items-start pr-10 w-full">
-                  <h1 className="pb-3 font-bold text-xl text-[var(--secundary)]">
-                    Descripción
-                  </h1>
+                  <h1 className="pb-3 font-bold text-xl text-[var(--secundary)]">Descripción</h1>
                   <h3 className="text-zinc-600 text-sm">
-                    {userData && userData.aboutMe.length == 0
-                      ? "Este usuario todavia no tiene descripción."
-                      : userData.aboutMe}
+                    {userInfo.aboutMe.length === 0 ? 'Este usuario todavia no tiene descripción.' : userInfo.aboutMe}
                   </h3>
                 </div>
               </div>
             </div>
 
             <div className="flex w-full flex-col">
-              <h1 className="pl-5 pb-3 text-xl font-bold text-[var(--secundary)]">
-                Publicaciones
-              </h1>
+              <h1 className="pl-5 pb-3 text-xl font-bold text-[var(--secundary)]">Publicaciones</h1>
               {userProducts && userProducts.length == 0 ? (
-                <NotFounded context={"profile"} />
+                <NotFounded context={'profile'} />
               ) : (
                 <div className="flex w-full justify-center items-center ">
                   <Cards products={userProducts} />
@@ -94,5 +51,5 @@ export default function Profile({ params }) {
       </div>
       <Footer />
     </>
-  );
+  )
 }
