5c412dd86b9f9e3d5e3f17385807ae4b1cebe887
tod1az <t.diaz.soto@gmail.com>
2023-12-07 19:49:15 -0300

    Delete fragments

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -9,23 +9,21 @@ export default async function Cards({ products }) {
   }
 
   return (
-    <>
-      <Suspense fallback={<LoadingPublication />}>
-        <div className="flex flex-wrap px-2 gap-4  items-center justify-center w-2/3">
-          {products.map((resp) => (
-            <Card
-              key={resp.id}
-              id={resp.id}
-              image={resp.image}
-              user={`${resp.user.name} ${resp.user.lastName}`}
-              price={resp.price}
-              title={resp.name}
-              userImage={resp.user.image}
-              userId={resp.userId}
-            />
-          ))}
-        </div>
-      </Suspense>
-    </>
+    <Suspense fallback={<LoadingPublication />}>
+      <div className="flex flex-wrap px-2 gap-4  items-center justify-center w-2/3">
+        {products.map((product) => (
+          <Card
+            key={product.id}
+            id={product.id}
+            image={product.image}
+            user={`${product.user.name} ${product.user.lastName}`}
+            price={product.price}
+            title={product.name}
+            userImage={product.user.image}
+            userId={product.userId}
+          />
+        ))}
+      </div>
+    </Suspense>
   )
 }
