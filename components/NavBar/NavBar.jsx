dd8e5baae07bbfc200932ca21ad3973d407b6d54
tod1az <t.diaz.soto@gmail.com>
2023-12-11 01:40:50 -0300

    delete unnecesary div

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -9,9 +9,7 @@ const NavBar = () => {
   return (
     <nav className="flex fixed top-0 w-full left-0 justify-between items-center h-[10vh] px-10 shadow-md bg-[var(--background)] z-50">
       <Link href="/">
-        <div className="flex content-center items-center ">
-          <Image src="/images/logo.svg" width={100} height={100} alt="logo" />
-        </div>
+        <Image src="/images/logo.svg" width={100} height={100} alt="logo" />
       </Link>
       <SearchBar />
       <NewPublication />
