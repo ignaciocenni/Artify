9c6347d76b11ab0fdef5bc64a0a1f560f7b4a469
tod1az <t.diaz.soto@gmail.com>
2023-12-11 11:09:42 -0300

    fix conditional chaining missing on user token

--- components/dashboard/CardsMetrics.jsx
+++ components/dashboard/CardsMetrics.jsx
@@ -1,86 +1,78 @@
-import ProductStatus from "./ProductStatus";
-import TotalSells from "./TotalSells";
-import pending from "../../public/images/pendingIcon.svg";
-import invisible from "../../public/images/invisibleIcon.svg";
-import visible from "../../public/images/visibleIcon.svg";
-import DoughnutChart from "./DoughnutChart";
-import backgroundColors from "./backgroundColors";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import LoadingData from "./LoadingData";
-import TotalUsers from "./TotalUsers.jsx";
+'use client'
+import ProductStatus from './ProductStatus'
+import TotalSells from './TotalSells'
+import pending from '../../public/images/pendingIcon.svg'
+import invisible from '../../public/images/invisibleIcon.svg'
+import visible from '../../public/images/visibleIcon.svg'
+import DoughnutChart from './DoughnutChart'
+import backgroundColors from './backgroundColors'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import LoadingData from './LoadingData'
+import TotalUsers from './TotalUsers.jsx'
+import { useSession } from 'next-auth/react'
 
 const getStats = async (session) => {
-  let stats;
-  if (session.user.role === "USER") {
-    stats = (await axios.get(`api/stats/${session && session.user.id}`)).data;
+  let stats
+  if (session.user.role === 'USER') {
+    stats = (await axios.get(`api/stats/${session && session.user.id}`)).data
   } else {
-    stats = (await axios.get("api/stats")).data;
+    stats = (await axios.get('api/stats')).data
   }
 
-  return stats;
-};
+  return stats
+}
 
-const CardsMetrics = (params) => {
-  const { session } = params;
-
-  const [stats, setStats] = useState(null);
+const CardsMetrics = () => {
+  const { data: session } = useSession()
+  const [stats, setStats] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const statsData = await getStats(session);
-        setStats(statsData);
+        const statsData = await getStats(session)
+        setStats(statsData)
       } catch (error) {
-        console.error("Error al obtener los datos:", error);
+        console.error('Error al obtener los datos:', error)
       }
-    };
+    }
 
-    fetchData();
-  }, [session]);
-
+    fetchData()
+  }, [session])
 
   let noDataAvailable
   let provincesData
-  
-  if (session.user.role === "USER") {
-    
-    noDataAvailable = stats?.userProvinceSales
-      ? Object.values(stats.userProvinceSales).every((value) => value === 0)
-      : true;
-  
+
+  if (session?.user.role === 'USER') {
+    noDataAvailable = stats?.userProvinceSales ? Object.values(stats.userProvinceSales).every((value) => value === 0) : true
+
     provincesData = {
       labels: stats?.userProvinceSales && Object.keys(stats.userProvinceSales),
       datasets: [
         {
-          label: "Ventas en la provincia",
-          data:
-            stats?.userProvinceSales && Object.values(stats.userProvinceSales),
+          label: 'Ventas en la provincia',
+          data: stats?.userProvinceSales && Object.values(stats.userProvinceSales),
           backgroundColor: backgroundColors,
-          borderWidth: 1,
-        },
-      ],
-    };
+          borderWidth: 1
+        }
+      ]
+    }
   } else {
-    noDataAvailable = stats?.provinceSales
-    ? Object.values(stats.provinceSales).every((value) => value === 0)
-    : false;
+    noDataAvailable = stats?.provinceSales ? Object.values(stats.provinceSales).every((value) => value === 0) : false
 
-  provincesData = {
-    labels: stats?.provinceSales && Object.keys(stats.provinceSales),
-    datasets: [
-      {
-        label: "Ventas en la provincia",
-        data:
-          stats?.provinceSales && Object.values(stats.provinceSales),
-        backgroundColor: backgroundColors,
-        borderWidth: 1,
-      },
-    ],
-  };
-    
+    provincesData = {
+      labels: stats?.provinceSales && Object.keys(stats.provinceSales),
+      datasets: [
+        {
+          label: 'Ventas en la provincia',
+          data: stats?.provinceSales && Object.values(stats.provinceSales),
+          backgroundColor: backgroundColors,
+          borderWidth: 1
+        }
+      ]
+    }
   }
-  
+
   return (
     <div className="flex w-full bg-[var(--primary)] h-full p-20 gap-20">
       <div className="flex flex-col w-1/2 bg-white rounded-3xl shadow-md shadow-zinc-400 p-20 justify-center items-center">
@@ -95,19 +87,13 @@ const CardsMetrics = (params) => {
       </div>
 
       <div className="flex flex-col justify-center w-1/2 gap-10">
-        {session && session.user.role === "USER" ? (
+        {session && session.user.role === 'USER' ? (
           <>
-            <TotalSells
-              total={stats ? `$${stats.totalSales}` : <LoadingData />}
-            />
+            <TotalSells total={stats ? `$${stats.totalSales}` : <LoadingData />} />
           </>
         ) : (
           <>
-            <TotalUsers
-              total={
-                stats && stats.totalUsers ? stats.totalUsers : <LoadingData />
-              }
-            />
+            <TotalUsers total={stats && stats.totalUsers ? stats.totalUsers : <LoadingData />} />
           </>
         )}
 
@@ -116,7 +102,7 @@ const CardsMetrics = (params) => {
           <div className="flex justify-center gap-2">
             <ProductStatus
               quantity={
-                session && session.user.role === "USER" ? (
+                session && session.user.role === 'USER' ? (
                   stats ? (
                     stats.productUserStatus.ACTIVE
                   ) : (
@@ -129,11 +115,11 @@ const CardsMetrics = (params) => {
                 )
               }
               image={visible}
-              text={"Productos Activos"}
+              text={'Productos Activos'}
             />
             <ProductStatus
               quantity={
-                session && session.user.role === "USER" ? (
+                session && session.user.role === 'USER' ? (
                   stats ? (
                     stats.productUserStatus.INACTIVE
                   ) : (
@@ -146,11 +132,11 @@ const CardsMetrics = (params) => {
                 )
               }
               image={invisible}
-              text={"Productos Inactivos"}
+              text={'Productos Inactivos'}
             />
             <ProductStatus
               quantity={
-                session && session.user.role === "USER" ? (
+                session && session.user.role === 'USER' ? (
                   stats ? (
                     stats.productUserStatus.PENDING
                   ) : (
@@ -163,13 +149,13 @@ const CardsMetrics = (params) => {
                 )
               }
               image={pending}
-              text={"Productos Pendientes"}
+              text={'Productos Pendientes'}
             />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CardsMetrics;
+export default CardsMetrics
